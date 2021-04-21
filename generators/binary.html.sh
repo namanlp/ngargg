64bb9105be30df20591370ef4f6fab5c75d78626
ngargg <naman@garudalinux.org>
2021-04-21 19:16:44 +0530

    Switch to Blackarch Site

---
+++ generators/binary.html.sh
@@ -0,0 +1,70 @@
+cat common/start-binary
+
+cat << EOF
+
+    <!-- Page Content -->
+    <div class="container result">
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="page-header page-header-title">
+	            <h1>Binary</h1> <h2>The list</h2>
+                </div>
+                <ol class="breadcrumb">
+                    <li><a class=home href="index.html">Home</a>
+                    </li>
+                    <li class="active"><a class=tools href="tools.html">tools</a></li>
+                     <li class="active">Binary</li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Content Row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <p>
+				Packages that operate on binary  les in some form.</p>
+                <br />
+                <p>
+                    <b>Tool count:</b> <a href="">${BINARY_COUNT}</a>
+                    <input type="text" id="searchTools" onkeyup="searchTools()" placeholder="Input tool name" title="Type in a name">
+                </p>
+                <div class="panel panel-default text-left">
+                    <div class="panel-heading">
+                        <b>BlackArch binary</b>
+                    </div>
+                    <table id="tbl-minimalist" itemscope itemtype="https://www.schema.org/Thing">
+                        <thead>
+                            <tr>
+                                <th class=tbl-name itemprop="item">Name</th>
+                                <th class=tbl-version>Version</th>
+                                <th class=tbl-description>Description</th>
+                                <th class=tbl-homepage>Homepage</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+EOF
+
+
+while IFS='|' read -r pkgname pkgver pkgdesc url ; do
+cat << EOF
+                            <tr>
+                                <td class=tbl-name itemprop="name">${pkgname}</td>
+                                <td class="tbl-version vcat">${pkgver}</td>
+                                <td class="tbl-description dcat" itemprop="description">${pkgdesc}</td>
+                                <td class=tbl-homepage itemprop="mainEntityOfPage"><a href="${url}" target="_blank">${url}</a></td>
+                            </tr>
+EOF
+done < data/binary
+
+cat <<\EOF
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+EOF
+
+cat common/end
---
+++ generators/fingerprint.html.sh
@@ -0,0 +1,70 @@
+cat common/start-fingerprint
+
+cat << EOF
+
+    <!-- Page Content -->
+    <div class="container result">
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="page-header page-header-title">
+	            <h1>Fingerprint</h1> <h2>The list</h2>
+                </div>
+                <ol class="breadcrumb">
+                    <li><a class=home href="index.html">Home</a>
+                    </li>
+                    <li class="active"><a class=tools href="tools.html">tools</a></li>
+                     <li class="active">fingerprint</li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Content Row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <p>
+Packages that exploit fingerprint biometric equipment.</p>
+                <br />
+                <p>
+                    <b>Tool count:</b> <a href="">${FINGERPRINT_COUNT}</a>
+                    <input type="text" id="searchTools" onkeyup="searchTools()" placeholder="Input tool name" title="Type in a name">
+                </p>
+                <div class="panel panel-default text-left">
+                    <div class="panel-heading">
+                        <b>BlackArch fingerprint</b>
+                    </div>
+                    <table id="tbl-minimalist" itemscope itemtype="https://www.schema.org/Thing">
+                        <thead>
+                            <tr>
+                                <th class=tbl-name itemprop="item">Name</th>
+                                <th class=tbl-version>Version</th>
+                                <th class=tbl-description>Description</th>
+                                <th class=tbl-homepage>Homepage</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+EOF
+
+
+while IFS='|' read -r pkgname pkgver pkgdesc url ; do
+cat << EOF
+                            <tr>
+                                <td class=tbl-name itemprop="name">${pkgname}</td>
+                                <td class="tbl-version vcat">${pkgver}</td>
+                                <td class="tbl-description dcat" itemprop="description">${pkgdesc}</td>
+                                <td class=tbl-homepage itemprop="mainEntityOfPage"><a href="${url}" target="_blank"><i class="fas fa-external-link-alt fa-lg"></i></a></td>
+                            </tr>
+EOF
+done < data/fingerprint
+
+cat <<\EOF
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+EOF
+
+cat common/end
